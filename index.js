b5791bf06b0a4ab45c63bce435a6def28b210165
Tyler M <tmorse2222@gmail.com>
2022-08-23 15:56:34 -0700

    Created Classes for Employee, Manager, engineer, and intern

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 const fs = require(`fs`);
 const inquirer = require(`inquirer`);
 
+function Employee(name, id, email, role) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.role = role;
+};
+
+class Manager extends Employee {
+    constructor(name, id, email, role) {
+        super(name, id, email, role);
+        this.officeNumber = officeNumber;
+    }
+};
+
+class Engineer extends Employee {
+    constructor(name, id, email, role) {
+        super(name, id, email, role);
+        this.github = github;
+    }
+};
+
+class Intern extends Employee {
+    constructor(name, id, email, role) {
+        super(name, id, email, role);
+        this.school = school;
+    }
+};
